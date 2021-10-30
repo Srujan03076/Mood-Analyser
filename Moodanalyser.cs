f757100b66a60ce8d7aa4794db2bb4439197e668
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-31 00:07:11 +0530

    Create ModeAnalyser Object

---
+++ Moodanalyser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSTestMoodAnalyser
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyser()
+        {
+        }
+        public string analyseMood()
+        {
+            if (this.message.Contains("sad"))
+                return "SAD";
+            else if (this.message.Contains("happy"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSTestMoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser");
+        }
+    }
+}
