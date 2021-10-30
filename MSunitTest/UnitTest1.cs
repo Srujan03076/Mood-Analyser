67b7599e84526ebf0c91af1f1c950bd7e2d46537
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-31 00:21:20 +0530

    Create ModeAnalyser Object

---
+++ MSunitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+using MSTestMoodAnalyser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestMoodAnalyser
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenMoodHappy_ShouldReturnHappy()
+        {
+            MoodAnalyser obj = new MoodAnalyser("I am in happy mood");
+            string result = obj.analyseMood();
+            Assert.AreEqual("SAD", result);
+        }
+
+        [TestMethod]
+        public void GivenMoodSad_ShouldReturnSad()
+        {
+            MoodAnalyser obj = new MoodAnalyser("I am in sad mood");
+            string result = obj.analyseMood();
+            Assert.AreEqual("SAD", result);
+        }
+    }
+
+}
